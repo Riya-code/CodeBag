5ac943572702a5b550a8d0635fa21bd795e333c8
Riya-code <56725036+Riya-code@users.noreply.github.com>
2020-10-20 03:55:43 +0530

    Add files via upload
    left & right shift

---
+++ Algo/shift.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+  
+
+int main() 
+{ 
+    // a = 5(00000101), b = 9(00001001) 
+    unsigned char a = 5, b = 9;  
+  
+    // The result is 00000010  
+       
+    printf("a<<1 = %d\n", a<<1); //a*(2*1)
+    printf("a>>2 = %d\n", a>>2); 
+    
+    // The result is 00000100 
+    printf("b>>1 = %d\n", b>>1);   
+    return 0; 
+} 
